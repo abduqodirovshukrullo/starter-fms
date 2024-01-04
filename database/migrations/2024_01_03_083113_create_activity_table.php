28768180a0a982e89d519b8258206191a83fbdf5
Shukurillo Abdukodirov <abduqodirov0101@gmail.com>
2024-01-04 10:47:52 +0900

    migration

--- database/migrations/2024_01_03_083113_create_activity_table.php
+++ database/migrations/2024_01_03_083113_create_activity_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('activity', function (Blueprint $table) {
+        Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('status');
