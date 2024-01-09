f38ea972d9f8fa0478e92a0333e29bbba03131d9
Shukurillo Abdukodirov <abduqodirov0101@gmail.com>
2024-01-09 19:20:53 +0900

    ++

--- app/Http/Controllers/Kafka/KafkaController.php
+++ app/Http/Controllers/Kafka/KafkaController.php
@@ -11,10 +11,10 @@ use Junges\Kafka\Message\Message;
 class KafkaController extends Controller
 {
     public function produce(Request $request){
-        $conf = new RdKafka\Conf();
+        $conf = new \RdKafka\Conf();
         $conf->set('log_level', (string) LOG_DEBUG);
         $conf->set('debug', 'all');
-        $rk = new RdKafka\Producer($conf);
+        $rk = new \RdKafka\Producer($conf);
         $rk->addBrokers("127.0.0.1:9092");
         $topic = $rk->newTopic("first_test");
         $topic->produce(RD_KAFKA_PARTITION_UA, 0, "Message payload");
