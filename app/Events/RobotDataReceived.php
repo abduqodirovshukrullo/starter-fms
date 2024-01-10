ab5fe12e804115ca6e2a351e513fd507af1e352f
Shukurillo Abdukodirov <abduqodirov0101@gmail.com>
2024-01-10 15:53:40 +0900

    ++

--- app/Events/RobotDataReceived.php
+++ app/Events/RobotDataReceived.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class RobotDataReceived
+class RobotDataReceived implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
