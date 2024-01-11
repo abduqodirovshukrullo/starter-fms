c497daf3e060fe23ae6a5a040d0f2f5c95aa2b95
Shukurillo Abdukodirov <abduqodirov0101@gmail.com>
2024-01-11 17:57:11 +0900

    ++

--- public/build/assets/createUrl-4ac06473.js
+++
@@ -1,1 +0,0 @@
-import{s as f}from"./index-51d05da2.js";import{a5 as b,bd as e}from"./main-8f4bf164.js";const s=(c,r)=>b(()=>{if(!(r!=null&&r.query))return e(c);const a=e(c),t=e(r==null?void 0:r.query),u=Object.fromEntries(Object.entries(t).map(([m,y])=>[m,e(y)]));return`${a}${u?`?${f(u)}`:""}`});export{s as c};
--- public/build/assets/useAbility-b1750d10.js
+++
@@ -1,1 +0,0 @@
-import{aF as i}from"./main-8f4bf164.js";const t=()=>i();export{t as u};
